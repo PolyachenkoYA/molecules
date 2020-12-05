f4441551d938b921ce9586487f8a03cc55190068
Yura <polyachenko.yua@phystech.edu>
2020-12-05 22:53:17 +0300

    pipeline from the guide works

--- !COMP/!go/full_cycle.py
+++ !COMP/!go/full_cycle.py
@@ -14,20 +14,21 @@ import mylib_molecules as my
 # Define a main() function that prints a little greeting.
 def main():
     args = sys.argv[1:]
-    if len(args) < 1:
-        print('usage: ' + sys.argv[0] + '     model_name     [keys,    N0,     N1]')
+    if len(args) < 2:
+        print('usage: ' + sys.argv[0] + '     model_name     input_params_name   [keys,    N0,     N1]')
         sys.exit(1)
         
     model_name = args[0]
-    if(len(args) > 1):
-        keys = args[1]
+    input_params_name = args[1]
+    if(len(args) > 2):
+        keys = args[2]
         if(keys == '-all'):
             keys = '-gen' + my.all_proc_flags
     else:
         keys = my.all_proc_flags
     keys = keys.split('-')
     
-    my.full_cycle_fnc(model_name, keys, args)
+    my.full_cycle_fnc(input_params_name, model_name, keys, args)
         
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
